179ca0ceb2bc0a9a00c450398453bcb35df04d4b
FlashCat <FlashCat@users.noreply.github.com>
2015-04-17 15:43:43 +0000

    rebuild pages at 2155a5d

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['lru_cache'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='lru_cache/struct.LruCache.html' title='lru_cache::LruCache'>LruCache</a>&lt;K, V&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'a, K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='lru_cache/struct.Iter.html' title='lru_cache::Iter'>Iter</a>&lt;'a, K, V&gt;",];
+implementors['lru_cache'] = ["impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='lru_cache/struct.LruCache.html' title='lru_cache::LruCache'>LruCache</a>&lt;K, V&gt;","impl&lt;'a, K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='lru_cache/struct.Iter.html' title='lru_cache::Iter'>Iter</a>&lt;'a, K, V&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
